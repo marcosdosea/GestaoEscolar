0e9537eb2f9aabf6480d59b12ac5136ba56dcdb6
Marcos Dósea <marcosdosea@yahoo.com.br>
2021-02-10 17:41:31 -0300

    remoção de bibliotecas desnecessária geração do Identity configuração dos projetos para o .NET 5

---
+++ Código/SiCAEWeb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SiCAEWeb.Areas.Identity.Data;
+
+[assembly: HostingStartup(typeof(SiCAEWeb.Areas.Identity.IdentityHostingStartup))]
+namespace SiCAEWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityContextConnection")));
+
+                services.AddDefaultIdentity<Usuario>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IdentityContext>();
+            });
+        }
+    }
+}
--- Código/SiCAEWeb/Models/DisciplinaModel.cs
+++ Código/SiCAEWeb/Models/DisciplinaModel.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
-using Org.BouncyCastle.Asn1.Cms;
-using Xamarin.Forms;
+﻿using System.ComponentModel.DataAnnotations;
 
 
 namespace SiCAEWeb.Models
 {
-    public class DisciplinaModel
-    {
+	public class DisciplinaModel
+	{
 
-        [Display(Name = "Código")]
-        [Required(ErrorMessage = "Código requerido")]
-        public int IdDisciplina { get; set; }
+		[Display(Name = "Código")]
+		[Required(ErrorMessage = "Código requerido")]
+		public int IdDisciplina { get; set; }
 
-        [Required(ErrorMessage = "Nome requerido")]
-        [StringLength(45,MinimumLength = 5, ErrorMessage = "Nome da disciplina é muito grande")]
-        public string Nome { get; set; }
-
-
-
-    }
-
+		[Required(ErrorMessage = "Nome requerido")]
+		[StringLength(45, MinimumLength = 5, ErrorMessage = "Nome da disciplina é muito grande")]
+		public string Nome { get; set; }
+	}
 }
