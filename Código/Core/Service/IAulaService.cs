03e05490130b21460806c239319e8831e0ef6924
Samuel <muelsam98@gmail.com>
2021-05-03 17:15:16 -0300

    Adição de CRUD Manter Conteudo
    Criação do caso de uso manter conteúdo
    Adição de comentarios de bloco em Escola e turma
    Close #224

---
+++ Código/Core/Service/IAulaService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.DTO;
+
+namespace Core.Service
+{
+    public interface IAulaService
+    {
+        void Editar(Aula aula);
+        int Inserir(Aula aula);
+        Aula Obter(int idAula);
+        IEnumerable<Aula> ObterPorNome(string nome);
+        IEnumerable<Aula> ObterTodos();
+        void Remover(int idAula);
+        IEnumerable<AulaDTO> ObterPorNomeOrdenadoDescending(string nome);
+    }
+}
---
+++ Código/SiCAEWeb/Controllers/AulaController.cs
@@ -0,0 +1,97 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SiCAEWeb.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SiCAEWeb.Controllers
+{
+    public class AulaController : Controller
+    {
+        // GET: AulaController1
+        IAulaService _aulaService;
+        IMapper _mapper;
+
+        public AulaController(IAulaService aulaService, IMapper mapper)
+        {
+            _aulaService = aulaService;
+            _mapper = mapper;
+        }
+
+        // GET: CidadeController
+        public ActionResult Index()
+        {
+            var listaAulas = _aulaService.ObterTodos();
+            var listaAulasModel = _mapper.Map<List<AulaModel>>(listaAulas);
+            return View(listaAulasModel);
+        }
+
+        // GET: CidadeController/Details/5
+        public ActionResult Details(int id)
+        {
+            Aula aula = _aulaService.Obter(id);
+            AulaModel aulaModel = _mapper.Map<AulaModel>(aula);
+            return View(aulaModel);
+        }
+
+        // GET: CidadeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CidadeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AulaModel aulaModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var aula = _mapper.Map<Aula>(aulaModel);
+                _aulaService.Inserir(aula);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: CidadeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Aula aula = _aulaService.Obter(id);
+            AulaModel aulaModel = _mapper.Map<AulaModel>(aula);
+            return View(aulaModel);
+        }
+
+        // POST: CidadeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, AulaModel aulaModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var aula = _mapper.Map<Aula>(aulaModel);
+                _aulaService.Editar(aula);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: CidadeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Aula aula = _aulaService.Obter(id);
+            AulaModel aulaModel = _mapper.Map<AulaModel>(aula);
+            return View(aulaModel);
+        }
+
+        // POST: CidadeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            _aulaService.Remover(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
